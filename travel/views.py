de9c3589d967214b235d037611e26b538bb9ee1a
Vlad Gerasimov <gerasimov.vlad2012@gmail.com>
2021-01-06 15:51:12 +0500

    add base.html, model, view cities

--- travel/views.py
+++ travel/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def home(request):
     name = 'Bob'
     return render(request, 'home.html', {'name': name})
+
+def about(request):
+    about = 'About'
+    return render(request, 'about.html', {'about': about})
